82c27c760a10f96e0268638b91087aa3a7a97a1a
Timothy Ha <timothyha@gmail.com>
2014-12-07 23:50:00 +0300

    Solved by adding lists of digits instead of numbers

--- 0021_0030/problem_0025.py
+++ 0021_0030/problem_0025.py
@@ -1,4 +1,4 @@
-# https://projecteuler.net/problem=20
+# https://projecteuler.net/problem=25
 # The Fibonacci sequence is defined by the recurrence relation: Fn = Fn-1 + Fn-2, where F1 = 1 and F2 = 1.
 # Hence the first 12 terms will be:
 # F1 = 1 F2 = 1 F3 = 2 F4 = 3 F5 = 5 F6 = 8 F7 = 13 F8 = 21 F9 = 34 F10 = 55 F11 = 89 F12 = 144
@@ -35,4 +35,3 @@ if __name__ == '__main__':
             break
         a = b[:]
         b = c[:]
-        
